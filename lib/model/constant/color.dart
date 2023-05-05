47aac96fdfd032fcfe0dc8fc64f30835aa49c531
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-05-06 08:22:21 +0900

    constant作成

---
+++ lib/model/constant/color.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+class NowColors {
+  static const Color headerBackgroundColor = Color(0xFFEEEEEE);
+  static const Color headerItemColor = Color(0xFF666666);
+  static const Color headerFavoriteIconColor = Color(0xFFEE7711);
+  static const Color headerBorderColor = Color(0xFFDCDDDD);
+  static const Color footerBackgroundColor = Color(0xCCFFFFFF);
+  static const Color copyrightBackgroundColor = Color(0xFFF6F6F6);
+  static const Color red = Color(0xFFFF0004);
+  static const Color markerTop = Color(0x00FFFFFF);
+  static const Color markerBottom = Color(0xFFEAEA55);
+  static const Color buttonTextColor = Color(0xFFFFFFFF);
+  static const Color backButtonColor = Color(0xFF555555);
+  static const Color homeButtonColor = Color(0xFF999999);
+  static const Color favoriteButtonColor = Color(0xFFFCAF22);
+  static const Color isFavoriteButtonColor = Color(0xFFE56604);
+  static const Color testButtonColor = Color(0xFFFF4500);
+  static const Color toAnswerButtonColor = Color(0xFFCCCCCC);
+  static const Color filledAnswerButtonColor = Color(0xffff4500);
+  static const Color goalBoxColor = Color(0xFFED9A00);
+  static const Color attentionBoxColor = Color(0xFFFF0000);
+  static const Color checkBoxColor = Color(0xFF999999);
+  static const Color h3BorderColor = Color(0xFFDCDDDD);
+  static const Color subtitle2BorderColor = Color(0xFF3E3A39);
+  static const Color attentionIconColor = Color(0xFFFCAF22);
+  static const Color checkBoxIconColor = Color(0xFF004EA2);
+  static const Color toPageTopBackgroundColor = Color(0xFFEEEEEE);
+  static const Color searchExpandedTitleColor = Color(0xFFAE2D1C);
+  static const Color htmlTitleColor1 = Color(0xFF4cc0ca);
+  static const Color htmlTitleColor2 = Color(0xFFb2e4e8);
+  static const Color htmlfontColor = Color(0xFFffffff);
+}
