ca0efdf480c79b094daaa22c47d5041d02b11007
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-05-07 00:10:34 +0900

    fix nabbotton

--- lib/widgets/bottomNavBarWidget.dart
+++ lib/widgets/bottomNavBarWidget.dart
@@ -10,27 +10,35 @@ class BottomNavBarWidget extends StatelessWidget {
     return BottomNavigationBar(
       items: [
         BottomNavigationBarItem(
-            label: '相手探し',
+            label: '募集欄',
             icon: IconButton(
               icon: const Icon(Icons.wc),
               onPressed: () {
                 // Navigator.pushNamed(context, '/looking');
               },
             )),
-        BottomNavigationBarItem(
-            label: 'いいねリスト',
-            icon: IconButton(
-              icon: const Icon(Icons.volunteer_activism),
-              onPressed: () {
-                // Navigator.pushNamed(context, '/chance');
-              },
-            )),
+        // BottomNavigationBarItem(
+        //     label: 'いいねリスト',
+        //     icon: IconButton(
+        //       icon: const Icon(Icons.volunteer_activism),
+        //       onPressed: () {
+        //         // Navigator.pushNamed(context, '/chance');
+        //       },
+        //     )),
         BottomNavigationBarItem(
             label: 'メッセージ',
             icon: IconButton(
               icon: const Icon(Icons.message),
               onPressed: () {
                 // Navigator.pushNamed(context, '/message');
+              },
+            )),
+        BottomNavigationBarItem(
+            label: 'アカウント',
+            icon: IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () {
+                Navigator.pushNamed(context, '/my-profile');
               },
             )),
       ],
